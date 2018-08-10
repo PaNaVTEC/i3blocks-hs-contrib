67c8304df36e5d4624f6960e5d56f7541268ce2f
Christian Panadero <panavtec@gmail.com>
2018-08-10 20:21:10 +0100

    add cpu click button

--- Cpu.hs
+++ Cpu.hs
@@ -11,6 +11,12 @@ main :: IO ()
 main = sh $ do
   idle <- idleCpu
   liftIO $ putStrLn $ "\61668 " ++ formatFloatN (100 - idle) 2 ++ "%"
+  button <- currentButton
+  handleButton button
+
+handleButton :: MonadIO io => Maybe Button -> io ExitCode
+handleButton Nothing = return ExitSuccess
+handleButton (Just LeftClick) = shell "urxvt -title pop-up -e htop" empty
 
 idleCpu :: Shell Double
 idleCpu = read . unpack . strip <$> (strict $ inshell (pack "iostat -o JSON | jq -r '.sysstat.hosts[0].statistics[0].\"avg-cpu\".idle'") empty)
