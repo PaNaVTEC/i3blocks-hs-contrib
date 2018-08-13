7ee60078c4ac900e61a4bde747104839ca09dffc
Christian Panadero <panavtec@gmail.com>
2018-08-13 21:43:31 +0100

    fix volume mute

--- Volume.hs
+++ Volume.hs
@@ -3,9 +3,9 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Data.Text (pack, unpack, strip)
-import           Turtle
 import           Common
+import           Data.Text (pack, strip, unpack)
+import           Turtle
 
 main :: IO ()
 main = sh $ do
@@ -16,14 +16,14 @@ main = sh $ do
   handleButton button
 
 handleButton :: MonadIO io => Maybe Button -> io ExitCode
-handleButton Nothing = return ExitSuccess
-handleButton (Just LeftClick) = shell "pavucontrol" empty
+handleButton Nothing           = return ExitSuccess
+handleButton (Just LeftClick)  = shell "pavucontrol" empty
 handleButton (Just RightClick) = shell "ponymix toggle >/dev/null" empty
-handleButton (Just WheelUp) = shell "ponymix increase 5 >/dev/null" empty
-handleButton (Just WheelDown) = shell "ponymix decrease 5 >/dev/null" empty
+handleButton (Just WheelUp)    = shell "ponymix increase 5 >/dev/null" empty
+handleButton (Just WheelDown)  = shell "ponymix decrease 5 >/dev/null" empty
 
 formatVol :: Bool -> Integer -> String
-formatVol True _ = "\61478" ++ " " ++ "x"
+formatVol True _    = "\61478 x"
 formatVol False vol = icon vol ++ " " ++ show vol ++ "%"
 
 icon :: Integer -> String
@@ -31,7 +31,7 @@ icon v | v < 33 = "\61479"
 icon _ = "\61480"
 
 isMuted :: Shell Bool
-isMuted = (/= ExitSuccess) <$> shell (pack "ponymix is-muted") empty
+isMuted = (== ExitSuccess) <$> shell (pack "ponymix is-muted") empty
 
 getVolume :: Shell Integer
 getVolume = toVolumeInt <$> (strict $ inshell (pack "ponymix get-volume") empty)
