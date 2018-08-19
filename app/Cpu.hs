a878b4772d0d923ff98f22e9bc7cfd8f5d4a701d
Carlos Morera de la Chica <carlos@codurance.com>
2018-08-19 13:38:49 +0100

    Refactor Cpu module

--- app/Cpu.hs
+++ app/Cpu.hs
@@ -6,14 +6,17 @@ import           Turtle
 
 main :: IO ()
 main = sh $ do
-  idle <- idleCpu
-  liftIO $ putStrLn $ "\61668 " ++ formatFloatN (100 - idle) 2 ++ "%"
-  button <- currentButton
-  handleButton button
+  liftIO . putStrLn =<< cpuUsage
+  maybe (return ExitSuccess) handleButton =<< currentButton
 
-handleButton :: MonadIO io => Maybe Button -> io ExitCode
-handleButton (Just LeftClick) = shell "urxvt -title pop-up -e htop" empty
-handleButton _                = return ExitSuccess
+cpuUsage :: Shell String
+cpuUsage = idleCpu >>= return . format
+ where format idle = let usage = (formatFloatN (100 - idle) 2)
+                     in "\61668 " ++ usage ++ "%"
+
+handleButton :: MonadIO io => Button -> io ExitCode
+handleButton LeftClick = shell "urxvt -title pop-up -e htop" empty
+handleButton _         = return ExitSuccess
 
 idleCpu :: Shell Double
 idleCpu = read . unpack . strip <$> (strict $ inshell (pack "iostat -o JSON | jq -r '.sysstat.hosts[0].statistics[0].\"avg-cpu\".idle'") empty)
